af215b34ffefd155978d84a0162185004754b57d
kaklrol65 <kaklrol65@gmail.com>
2026-08-22 20:36:06 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='habitos-final-v2';const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='habitos-final-v2-1';
+const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
