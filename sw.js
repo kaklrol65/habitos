46e0ac8db7b45fd32d8c7ffacd7e51c836ad2bcf
kaklrol65 <kaklrol65@gmail.com>
2026-09-18 22:48:13 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-const C='habitos-final-v5-final';
-const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(
- caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('habitos-final-')&&k!==C).map(k=>caches.delete(k))))
- .then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='habitos-beta1-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/nico.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
