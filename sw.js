81051d4278a5e4d6f55d4e957e3efc143c00caa4
kaklrol65 <kaklrol65@gmail.com>
2026-09-22 19:04:27 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-const CACHE='habitos-beta1-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/nico.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
+const CACHE='habitos-beta1-v11';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/nico.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  // Always prefer the network for the HTML so installed PWAs can receive new builds.
+  if(event.request.mode==='navigate' || url.pathname.endsWith('/index.html')){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put('./index.html',copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached || fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+    return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
